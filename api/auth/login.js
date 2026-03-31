95c728cfad800caaa46bf5caa667402796acd46f
dyno786 <mohammedadris108@gmail.com>
2026-03-31 13:31:48 +0100

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,1 +1,22 @@
+export default function handler(req, res) {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const redirectUri = process.env.REDIRECT_URI || `https://${req.headers.host}/api/auth/callback`;
 
+  const scopes = [
+    'https://www.googleapis.com/auth/webmasters.readonly',
+    'https://www.googleapis.com/auth/analytics.readonly',
+    'https://www.googleapis.com/auth/adwords',
+    'openid','email','profile'
+  ].join(' ');
+
+  const params = new URLSearchParams({
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    response_type: 'code',
+    scope: scopes,
+    access_type: 'offline',
+    prompt: 'consent',
+  });
+
+  res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${params}`);
+}
